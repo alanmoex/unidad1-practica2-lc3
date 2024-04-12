7f38249ee341061542b41a16bb6052d7d3aec6e9
alanmoex <alan.moex@gmail.com>
2024-04-12 10:33:59 -0300

    removed BeersList

--- lc3-u1-p2/src/App.jsx
+++ lc3-u1-p2/src/App.jsx
@@ -1,10 +1,9 @@
 import Beers from "./components/beers/Beers";
-import BeersList from "./components/showBeers/showBeers";
 
 function App() {
   return (
     <>
-      <BeersList beers={<Beers />} />
+      <Beers></Beers>
     </>
   );
 }
